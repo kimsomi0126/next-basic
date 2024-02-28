a533d860fa91c2ff4a441854139ee88e341bdc35
kimsomi0126 <kimsomi00126@gmail.com>
2024-02-28 13:13:57 +0900

    docs : Next.js 기초

--- src/app/basic/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-  return <div>page입니다이야이야 </div>;
-};
-
-export default page;
---
+++ src/app/detail/[city]/page.tsx
@@ -0,0 +1,49 @@
+//"use client";
+// @은 /src 를 가르키는 절대 경로입니다.
+import style from "@/app/styles/detail.module.css";
+import HomeButton from "@/app/components/Homebutton";
+import { useRouter } from "next/navigation";
+// import Link from "next/link";
+import React from "react";
+
+// function ( parameter: 데이터 종류)
+// 1. const page = (받은값:Props) => {
+
+// function ( {속성:값}: 데이터 종류)
+// 2. const page = ({속성:값}:Props) => {
+
+// 약속된 속성명 params
+// 3. const page = ({params:params}:Props) => {
+
+// 축약형 적용
+// 4. const page = ({params}:Props) => {
+// type Props = {
+//   // 키명 : {속성:값종류};
+//   // params : {속성:값종류};
+//   // params : {city:값종류};
+//   // params: { city: string };
+// };
+
+type Props = {
+  params: {
+    city: string;
+  };
+};
+
+const Detail = ({ params }: Props) => {
+  const cityName = decodeURI(params.city);
+  // 첫 페이지로 이동
+  // 주의사항 : react-router-dom (X)
+  // import { useRouter } from "next/navigation";
+
+  return (
+    <>
+      <div className={style.detailTitle}>상세내용 : {cityName}</div>
+      {/* <Link href="/">이전페이지</Link> */}
+      {/* <button onClick={() => handleClick()}>이전으로</button> */}
+      <HomeButton />
+    </>
+  );
+};
+
+export default Detail;
